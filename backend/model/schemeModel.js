3deeead36f5793c32e9334613526475d76e20bdf
SkylerOnAir <blancdiable1@gmail.com>
2025-07-04 15:44:52 +0530

    Added the schemes model

---
+++ backend/model/schemeModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const schemeSchema = mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: [true, 'Please set the title'],
+			maxlength: 50,
+		},
+		shortDescription: {
+			type: String,
+			required: [true, 'Please set the short description'],
+			maxlength: 150,
+		},
+		benefits: {
+			type: String,
+			required: [true, 'Please set the benefits'],
+			maxlength: 250,
+		},
+		objective: {
+			type: String,
+			required: [true, 'Please set the objectives'],
+			maxlength: 250,
+		},
+		eligibility: {
+			type: String,
+			required: [true, 'Please set the eligibility criteria'],
+			maxlength: 250,
+		},
+		agency: {
+			type: String,
+			required: [true, 'Please set the agency overseeing the scheme'],
+			maxlength: 250,
+		},
+		longDescription: {
+			type: String,
+			required: [true, 'Please set the long description'],
+			maxlength: 550,
+		},
+	},
+	{ timestamps: true }
+);
+
+export const Scheme = mongoose.model('Scheme', schemeSchema);
