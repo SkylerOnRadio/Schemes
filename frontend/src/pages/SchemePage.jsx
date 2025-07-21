ed7edcd0a4516c4f7f636ca8344167ce5c0cfd7e
SkylerOnAir <blancdiable1@gmail.com>
2025-07-21 22:13:45 +0530

    Made SchemePage use components for better management of code

--- frontend/src/pages/SchemePage.jsx
+++ frontend/src/pages/SchemePage.jsx
@@ -1,10 +1,11 @@
+import EligibilityCard from '../components/EligibilityCard';
 import SchemeDetails from '../components/SchemeDetails';
 
 const SchemePage = () => {
 	return (
 		<div className="flex">
 			<SchemeDetails />
-			<div className="bg-white border w-95 mt-7 rounded-2xl">hello</div>
+			<EligibilityCard />
 		</div>
 	);
 };
