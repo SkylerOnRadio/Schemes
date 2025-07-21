f14f81fa770c1d052b74bbe18c57dcadbdffd77d
SkylerOnAir <blancdiable1@gmail.com>
2025-07-21 18:58:51 +0530

    Changed navbar to look better and added a user profile menu to it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,74 +1,109 @@
-import React, { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaLandmark, FaComment, FaTimes, FaBars } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
+import { FaHome, FaLandmark, FaComment } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 const Navbar = () => {
+	const { user } = useSelector((state) => state.auth);
+	const menuRef = useRef(null);
+
 	const dispatch = useDispatch();
 
-	const [menuOpen, setMenuOpen] = useState(false);
+	const [userMenuOpen, setUserMenuOpen] = useState(false);
 
-	const toggleMenu = () => {
-		setMenuOpen(!menuOpen);
+	const toggleUserMenu = () => {
+		setUserMenuOpen(!userMenuOpen);
 	};
+
+	const logoutUser = () => {
+		dispatch(logout());
+		dispatch(reset());
+	};
+
+	const userExists = () => {
+		if (user && user !== null) return true;
+		return false;
+	};
+
+	useEffect(() => {
+		function autoMenuClose(e) {
+			if (menuRef.current && !menuRef.current.contains(e.target)) {
+				setUserMenuOpen(false);
+			}
+		}
+
+		document.addEventListener('mousedown', autoMenuClose);
+		return () => {
+			document.removeEventListener('mousedown', autoMenuClose);
+		};
+	}, []);
 
 	return (
 		<>
-			<nav className="bg-gray-600 px-9 py-7 rounded-br-2xl rounded-bl-2xl  shadow-md ">
-				<div className="flex justify-between items-center ">
-					<button
-						onClick={toggleMenu}
-						className="text-white text-3xl focus:outline-none translate-y-8 translate-x-319 relative "
-					>
-						{menuOpen ? <FaTimes /> : <FaBars />}
-					</button>
-				</div>
-				<div className="flex gap-10  items-center justify-center">
+			<nav className="bg-gray-600 px-9 py-7 rounded-br-2xl rounded-bl-2xl shadow-md relative h-25  z-50">
+				<div className="flex justify-center gap-10 items-center mt-2">
 					<Link
 						to="/"
-						className="text-white font-bold text-xl px-2 py-4 rounded-xl flex items-center gap-2"
+						className="text-white font-bold text-xl flex items-center gap-2 hover:text-gray-400"
 					>
 						<FaHome /> Home
 					</Link>
 					<Link
 						to="/schemes"
-						className="text-white font-bold text-xl px-2 py-4 rounded-xl flex items-center gap-2"
+						className="text-white font-bold text-xl flex items-center gap-2 hover:text-gray-400"
 					>
 						<FaLandmark /> Schemes
 					</Link>
 					<Link
 						to="/feedbacks"
-						className="text-white font-bold text-xl px-2 py-4 rounded-xl flex items-center gap-2"
+						className="text-white font-bold text-xl flex items-center gap-2 hover:text-gray-400"
 					>
 						<FaComment /> Feedbacks
 					</Link>
 				</div>
 
-				{menuOpen && (
-					<div className="flex flex-col mt-5 gap-3 md:hidden">
-						<Link
-							to="/"
-							className="text-white font-bold text-lg px-2 py-2 rounded-md flex items-center gap-2"
-							onClick={() => setMenuOpen(false)}
-						>
-							<FaHome /> Home
-						</Link>
-						<Link
-							to="/schemes"
-							className="text-white font-bold text-lg px-2 py-2 rounded-md flex items-center gap-2"
-							onClick={() => setMenuOpen(false)}
-						>
-							<FaLandmark /> Schemes
-						</Link>
-						<Link
-							to="/feedbacks"
-							className="text-white font-bold text-lg px-2 py-2 rounded-md flex items-center gap-2"
-							onClick={() => setMenuOpen(false)}
-						>
-							<FaComment /> Feedbacks
-						</Link>
-					</div>
-				)}
+				<div
+					ref={menuRef}
+					className="absolute top-1/2 right-15 -translate-y-1/2"
+				>
+					<img
+						src="/images/user.jpg"
+						alt="user"
+						className="size-14 rounded-full border cursor-pointer"
+						onClick={toggleUserMenu}
+					/>
+					{userMenuOpen && (
+						<div className="absolute top-full mt-2 left-1/2 -translate-x-1/2 bg-gray-600 rounded shadow w-40 p-3">
+							{userExists() ? (
+								<div className="text-white text-lg text-center">
+									<h2 className="mb-2 font-semibold border-b border-gray-700">
+										{user.username}
+									</h2>
+									<h2 className="mb-2 hover:text-neutral-300">Details</h2>
+									<button
+										onClick={logoutUser}
+										className="text-red-400 hover:text-red-600"
+									>
+										Logout
+									</button>
+								</div>
+							) : (
+								<div className="text-white text-lg items-center text-center grid">
+									<h2 className="mb-2 font-semibold border-b border-gray-700">
+										User
+									</h2>
+									<Link to="/register" className="mb-2 hover:text-neutral-300">
+										Register
+									</Link>
+									<Link to="/login" className="hover:text-neutral-300">
+										Login
+									</Link>
+								</div>
+							)}
+						</div>
+					)}
+				</div>
 			</nav>
 		</>
 	);
